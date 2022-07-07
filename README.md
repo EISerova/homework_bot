9a65e652cde8048428d245f14157007522376d68
Екатерина Серова <katyaserova@yandex.ru>
2022-07-07 20:05:42 +0300

    change readme.md

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
-# Бот-ассистент
-## _Сделан в рамках учебы в Яндекс-Практикуме_
 
-![N|Solid](https://puremodplay.com/wp-content/uploads/2021/10/download-telegram-80x80.webp)![N|Solid](https://i116.fastpic.org/big/2022/0308/dc/1032585c66dfa90bae8e0197ac6ebbdc.jpg)
+# Homework bot
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) ![Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white) 
 
-## Обзор
-Telegram-бот, который обращается к API сервиса Практикум.Домашка и узнает статус домашней работы
 
-- Делает запрос к API (раз в 10 минут).
-- Проверяет ответ.
-- Если есть обновления, отправляет статус через сообщение в Telegram.
 
-## Технологии
+### Описание:
 
-При работе над ботом я использовала:
+Telegram-бот для отслеживания статуса домашней работы. Делает API-запрос раз в 10 минут. При изменении статуса в бот приходит соответствующее сообщение.
+Учебный проект, созданный в рамках учебы в Яндекс.Практикуме.
 
-- Python - бот написан на языке программирования Python.
-- Python-telegram-bot - Python-библиотека для работы с Bot API
+### Стэк технологий:
+Python-telegram-bot
+
+### Зависимости:
+
+Python-telegram-bot 13.7
+
+
+### Скриншоты:
+
+![App Screenshot](https://i120.fastpic.org/big/2022/0707/37/3cfa9601a85b50377108c99771de6537.jpg)
+
+### Автор: 
+Серова Екатерина
+
+### Обратная связь:
+
+Если у вас есть предложения или замечания, пожалуйста, свяжитесь со мной - katyaserova@yandex.ru
+
+
+### Лицензия:
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- homework.py
+++ homework.py
@@ -9,51 +9,50 @@ from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
 from requests.exceptions import RequestException
 
-from exceptions import (ResponseDeniedError, ResponseKeyError,
-                        ResponseStatusError)
+from exceptions import ResponseDeniedError, ResponseKeyError, ResponseStatusError
 
 load_dotenv()
 
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-TOKENS = ('PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID')
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TOKENS = ("PRACTICUM_TOKEN", "TELEGRAM_TOKEN", "TELEGRAM_CHAT_ID")
+ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
+HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
 RETRY_TIME = 600
 
 HOMEWORK_VERDICTS = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+    "approved": "Работа проверена: ревьюеру всё понравилось. Ура!",
+    "reviewing": "Работа взята на проверку ревьюером.",
+    "rejected": "Работа проверена: у ревьюера есть замечания.",
 }
 
 INFO_SEND_MESSAGE = 'Сообщение "{message}" отправлено в Telegram'
 ERROR_SEND_MESSAGE = 'Сообщение "{message}" не отправлено Telegram: {error}'
 ERROR_ENDPOINT = (
-    'Сервер вернул некорректный код ответа: {response},'
-    'параметры запроса: headers - {headers},'
-    'params – {params}, endpoint - {url}'
+    "Сервер вернул некорректный код ответа: {response},"
+    "параметры запроса: headers - {headers},"
+    "params – {params}, endpoint - {url}"
 )
 ERROR_GET_API = (
-    'Запрос не выполнен: {error},'
-    'параметры запроса: headers - {headers},'
-    'params – {params}, endpoint - {url}'
+    "Запрос не выполнен: {error},"
+    "параметры запроса: headers - {headers},"
+    "params – {params}, endpoint - {url}"
 )
-ERROR_RESPONSE_TYPE = 'json-ответ - не словарь, полученный тип данных: {type}'
+ERROR_RESPONSE_TYPE = "json-ответ - не словарь, полученный тип данных: {type}"
 ERROR_HOMEWORKS_TYPE = 'В json-ответе тип "homeworks" - не список, а: {type}'
-ERROR_RESPONSE_KEY = 'Получен некорректный ответ: нет ключа {key}'
-INFO_RESPONSE = 'Ответ от сервера получен'
-INFO_HOMEWORKS = 'В списке домашних работе нет обновлений'
-ERROR_STATUS = 'Неизвестный статус домашней работы: {status}'
+ERROR_RESPONSE_KEY = "Получен некорректный ответ: нет ключа {key}"
+INFO_RESPONSE = "Ответ от сервера получен"
+INFO_HOMEWORKS = "В списке домашних работе нет обновлений"
+ERROR_STATUS = "Неизвестный статус домашней работы: {status}"
 INFO_STATUS = 'Изменился статус проверки работы "{name}": {verdict}'
-CRITICAL_TOKEN = 'Отсутствует обязательный токен: {token}'
-ERROR_MAIN = 'Работа программы остановлена: {error}'
+CRITICAL_TOKEN = "Отсутствует обязательный токен: {token}"
+ERROR_MAIN = "Работа программы остановлена: {error}"
 RESPONSE_DENIED = (
-    'Запрос отклонен сервером, причина: {key} : {denied}.'
-    'Параметры запроса: headers - {headers},'
-    'params – {params}, endpoint - {url}'
+    "Запрос отклонен сервером, причина: {key} : {denied}."
+    "Параметры запроса: headers - {headers},"
+    "params – {params}, endpoint - {url}"
 )
 
 
@@ -70,26 +69,23 @@ def send_message(bot, message):
 
 def get_api_answer(current_timestamp):
     """Делает запрос к эндпоинту API-сервиса и возвращает ответ."""
-    params = {'from_date': current_timestamp}
+    params = {"from_date": current_timestamp}
     response_params = dict(url=ENDPOINT, headers=HEADERS, params=params)
     try:
         response = requests.get(**response_params)
     except RequestException as err:
-        raise ConnectionError(
-            ERROR_GET_API.format(error=err, **response_params)
-        )
+        raise ConnectionError(ERROR_GET_API.format(error=err, **response_params))
     if response.status_code != HTTPStatus.OK:
         raise ResponseStatusError(
-            ERROR_ENDPOINT.format(
-                response=response.status_code, **response_params
-            )
+            ERROR_ENDPOINT.format(response=response.status_code, **response_params)
         )
     response_json: dict = response.json()
-    for key in ['error', 'code']:
+    for key in ["error", "code"]:
         if key in response_json:
             raise ResponseDeniedError(
                 RESPONSE_DENIED.format(
-                    key=key, denied=response_json[key], **response_params)
+                    key=key, denied=response_json[key], **response_params
+                )
             )
     return response_json
 
@@ -97,16 +93,12 @@ def get_api_answer(current_timestamp):
 def check_response(response):
     """Проверяет корректность ответа API, возвращает список домашних работ."""
     if not isinstance(response, dict):
-        raise TypeError(
-            ERROR_RESPONSE_TYPE.format(type=type(response))
-        )
-    if 'homeworks' not in response:
-        raise ResponseKeyError(
-            ERROR_RESPONSE_KEY.format(key='homeworks')
-        )
-    if 'current_date' not in response:
-        log.error(ERROR_RESPONSE_KEY.format(key='current_date'))
-    homeworks: list = response.get('homeworks')
+        raise TypeError(ERROR_RESPONSE_TYPE.format(type=type(response)))
+    if "homeworks" not in response:
+        raise ResponseKeyError(ERROR_RESPONSE_KEY.format(key="homeworks"))
+    if "current_date" not in response:
+        log.error(ERROR_RESPONSE_KEY.format(key="current_date"))
+    homeworks: list = response.get("homeworks")
     if not isinstance(homeworks, list):
         raise TypeError(ERROR_HOMEWORKS_TYPE.format(type=type(homeworks)))
     log.info(INFO_RESPONSE)
@@ -115,8 +107,8 @@ def check_response(response):
 
 def parse_status(homework):
     """Извлекает статус проверки, возвращает строку для отправки."""
-    name: str = homework['homework_name']
-    status: str = homework['status']
+    name: str = homework["homework_name"]
+    status: str = homework["status"]
     if status not in HOMEWORK_VERDICTS:
         raise ValueError(ERROR_STATUS.format(status=status))
     return INFO_STATUS.format(name=name, verdict=HOMEWORK_VERDICTS[status])
@@ -143,7 +135,7 @@ def main():
             if homework:
                 if send_message(bot, parse_status(homework[0])):
                     current_timestamp: int = response.get(
-                        'current_date', current_timestamp
+                        "current_date", current_timestamp
                     )
         except Exception as error:
             text = ERROR_MAIN.format(error=error)
@@ -152,17 +144,15 @@ def main():
         time.sleep(RETRY_TIME)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """Настройки логгера."""
     log = logging.getLogger()
     log.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
-        '%(asctime)s - %(levelname)s - %(message)s - %(funcName)s - %(lineno)d'
+        "%(asctime)s - %(levelname)s - %(message)s - %(funcName)s - %(lineno)d"
     )
     file_handler = RotatingFileHandler(
-        __file__ + '.log',
-        maxBytes=50000000,
-        backupCount=5
+        __file__ + ".log", maxBytes=50000000, backupCount=5
     )
     stream_heandler = logging.StreamHandler()
     file_handler.setFormatter(formatter)
